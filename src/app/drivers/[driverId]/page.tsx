b4369485e5875616c14f783e2ab19ba41bb6e1f6
Dekk <49681302+dekana576@users.noreply.github.com>
2026-03-23 14:04:53 +0800

    add detail driver

---
+++ src/app/drivers/[driverId]/page.tsx
@@ -0,0 +1,10 @@
+import DetailDriverPage from "@/components/pages/driver/detail-driver-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Detail Driver | OTW TL",
+};
+
+export default function DetailDriver() {
+  return <DetailDriverPage />;
+}
