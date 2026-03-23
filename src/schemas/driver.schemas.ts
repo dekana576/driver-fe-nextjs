7d2cf7bb888fbd4e322ca8da2ebc5f07ae345d27
Dekk <49681302+dekana576@users.noreply.github.com>
2026-03-23 09:55:11 +0800

    create driver schema

--- src/schemas/driver.schemas.ts
+++ src/schemas/driver.schemas.ts
@@ -1,37 +1,37 @@
-import * as yup from "Yup";
+import * as yup from "yup";
 
 export const CreateDriverSchema = yup.object({
-    name: yup.string().required("Name is Required"),
+  name: yup.string().required("Name is Required"),
 
-    phoneNumber: yup
-        .number()
-        .test(
-            "is-number",
-            "phone number must be a number",
-            (value) => typeof value === "number"
-        )
-        .required("Phone Number is Required"),
+  phoneNumber: yup
+    .number()
+    .test(
+      "is-number",
+      "Phone Number must be a number",
+      (value) => typeof value === "number",
+    )
+    .required("Phone Number is Required"),
 
-    status: yup
-        .mixed<"AVAILABLE" | "ON_TRIP" | "OFFLINE"> ()
-        .oneOf(["AVAILABLE", "ON_TRIP", "OFFLINE"], "Invalid Status")
-        .required("Statu is Required"),
-    
-    vehicleType: yup
-        .mixed<"MOTORCYCLE" | "CAR"> ()
-        .oneOf(["MOTORCYCLE", "CAR"], "Invalid Vehicle Type")
-        .required("Vehicle Type is Required"),
-    
-    plateNumber: yup.string().required("Plate Number is Required"),
+  status: yup
+    .mixed<"AVAILABLE" | "ON_TRIP" | "OFFLINE">()
+    .oneOf(["AVAILABLE", "ON_TRIP", "OFFLINE"], "Invalid Status")
+    .required("Status is Required"),
 
-    rating: yup
-        .number()
-        .required("Rating is Required")
-        .min(1, "Rating must be atleast 1")
-        .max(5, "Rating cannot be more than 5"),
+  vehicleType: yup
+    .mixed<"MOTORCYCLE" | "CAR">()
+    .oneOf(["MOTORCYCLE", "CAR"], "Invalid Vehicle Type")
+    .required("Vehicle Type is Required"),
+
+  plateNumber: yup.string().required("Plate Number is Required"),
+
+  rating: yup
+    .number()
+    .typeError("Rating must be a Number")
+    .required("Rating is Required")
+    .min(1, "Rating must be atleast 1")
+    .max(5, "Rating cannot more than 5"),
 
     currentLocation: yup.string().required("Current Location is Required")
-
 });
 
 export const UpdateDriverSchema = CreateDriverSchema;
