45dda1167affa7500c4159a77ccd4087d453cfa4
Dekk <49681302+dekana576@users.noreply.github.com>
2026-03-23 13:49:25 +0800

    fix features driver

--- src/features/driver/use-get-driver-by-id.ts
+++ src/features/driver/use-get-driver-by-id.ts
@@ -1,9 +1,10 @@
 import { getDriverById } from "@/services/actions/driver.actions";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetDriverById = async (driverId: string) => {
+export const useGetDriverById = (driverId: string) => {
     return useQuery({
         queryKey: ["get-driver-by-id", driverId],
         queryFn: () => getDriverById(driverId)
     });
 };
+
--- src/features/driver/use-get-driver-list.ts
+++ src/features/driver/use-get-driver-list.ts
@@ -2,7 +2,7 @@ import { getDriverList } from "@/services/actions/driver.actions"
 import { useQuery } from "@tanstack/react-query";
 import { useDebounce } from "use-debounce";
 
-export const useGetDriverList = async (search: string) => {
+export const useGetDriverList = (search: string) => {
     const [debouncedSearch] = useDebounce(search, 500);
 
     return useQuery({
